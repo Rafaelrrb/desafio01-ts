3123492ed6800a4a905f522a3d26be585b3686d8
Rafaelrrb <rafaeborba@gmail.com>
2024-10-01 16:35:06 -0300

    refactor: Foi criado instancias para cada um dos tipos de conta no app.ts e executado os métodos possíveis.

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
+import { BonusAccount } from './class/BonusAccount'
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+peopleAccount.deposit(10)
+peopleAccount.withdraw(2)
 console.log(peopleAccount)
-peopleAccount.deposit()
+
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(20)
+companyAccount.getLoan(20)
 console.log(companyAccount)
+
+const bonusAccount: BonusAccount = new BonusAccount('Bonus', 30)
+bonusAccount.deposit(20)
+console.log(bonusAccount)
